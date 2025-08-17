dcdce12fca75a961303b2c9b2e679fabd22129d2
joe <joeitguy@gmail.com>
2025-08-17 16:55:55 -0700

    make it better

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>⚡ React is Alive ⚡</h1>
+        <p>Your frontend is up and running.</p>
+        <p>Ready to connect to Flask backend at <code>localhost:5000</code></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
